227bcaa546e43609a6e6579a664ea4c416663523
PRIYANSH SETH <114296355+INSANEWARRIORR@users.noreply.github.com>
2022-11-08 17:42:03 +0530

    Add files via upload

---
+++ FloppyBirdGame MOD ICT WPF/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Windows.Threading;
+
+namespace FloppyBirdGame_MOD_ICT_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer gameTimer = new DispatcherTimer();
+
+        double score;
+        int gravity = 8;
+        bool gameover;
+        Rect FlappyBirdHitBox;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            gameTimer.Tick += MainEventTimer;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            StartGame();
+        }
+
+        private void MainEventTimer(object sender, EventArgs e)
+        {
+            TxtScore.Content = "Score : " + score;
+
+            FlappyBirdHitBox = new Rect(Canvas.GetLeft(FlappyBird), Canvas.GetTop(FlappyBird), FlappyBird.Width - 12, FlappyBird.Height);
+
+            Canvas.SetTop(FlappyBird, Canvas.GetTop(FlappyBird) + gravity);
+
+            if (Canvas.GetTop(FlappyBird) < -30 || Canvas.GetTop(FlappyBird) + FlappyBird.Height > 460)
+            {
+                EndGame();
+            }
+
+
+            foreach (var x in MyCanvas.Children.OfType<Image>())
+            {
+                if ((string)x.Tag == "obs1" || (string)x.Tag == "obs2" || (string)x.Tag == "obs3")
+                {
+                    Canvas.SetLeft(x, Canvas.GetLeft(x) - 5);
+
+                    if (Canvas.GetLeft(x) < -100)
+                    {
+                        Canvas.SetLeft(x, 800);
+
+                        score += .5;
+                    }
+
+                    Rect PillarHitBox = new Rect(Canvas.GetLeft(x), Canvas.GetTop(x), x.Width, x.Height);
+
+                    if (FlappyBirdHitBox.IntersectsWith(PillarHitBox))
+                    {
+                        EndGame();
+                    }
+                }
+
+                if ((string)x.Tag == "clouds")
+                {
+                    Canvas.SetLeft(x, Canvas.GetLeft(x) - 1);
+
+                    if (Canvas.GetLeft(x) < -250)
+                    {
+                        Canvas.SetLeft(x, 550);
+
+                        score += .5;
+                    }
+                }
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                FlappyBird.RenderTransform = new RotateTransform(-20, FlappyBird.Width / 2, FlappyBird.Height / 2);
+                gravity = -8;
+            }
+             
+            if (e.Key == Key.R && gameover == true)
+            {
+                StartGame();
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            FlappyBird.RenderTransform = new RotateTransform(5, FlappyBird.Width / 2, FlappyBird.Height / 2);
+
+            gravity = 8;
+        }
+
+
+        private void StartGame()
+        {
+            MyCanvas.Focus();
+
+            int temp = 300;
+
+            score = 0;
+
+            gameover = false;
+
+            Canvas.SetTop(FlappyBird, 190);
+
+            foreach (var x in MyCanvas.Children.OfType<Image>())
+            {
+                if ((string)x.Tag == "obs1")
+                {
+                    Canvas.SetLeft(x, 500);
+                }
+                if ((string)x.Tag == "obs2")
+                {
+                    Canvas.SetLeft(x, 800);
+                }
+                if ((string)x.Tag == "obs3")
+                {
+                    Canvas.SetLeft(x, 1100);
+                }
+
+                if ((string)x.Tag == "clouds")
+                {
+                    Canvas.SetLeft(x, 300 + temp);
+                    temp = 800;
+                }
+            }
+            gameTimer.Start();
+        }
+
+        private void EndGame()
+        {
+            gameTimer.Stop();
+            gameover = true;
+            TxtScore.Content += " Game Over!!! Press R to restart.";
+        }
+    }
+}
+
+
+
+//**************************************************************************
+//                                             CODE  BY  PRIYANSH SETH
+//**************************************************************************
